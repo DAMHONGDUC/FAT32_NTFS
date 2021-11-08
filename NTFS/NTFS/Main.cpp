14070d2ff8fb6939958fb568fe444c67c0a245f2
DAMHONGDUC <hongduc001h@gmail.com>
2021-11-08 19:09:02 +0700

    Update feature Read Partition Boot Sector

---
+++ NTFS/NTFS/Main.cpp
@@ -0,0 +1,29 @@
+﻿#include "NTFS.h"
+
+int main(int argc, char** argv)
+{
+    // xử lí nhập tên ổ đĩa
+    wstring disk_name;
+    cout << "Nhap ten o dia: ";
+    wcin >> disk_name;
+    disk_name = L"\\\\.\\" + disk_name + L":";
+    LPCWSTR drive = disk_name.c_str();
+
+    // xử lí đọc
+    BYTE *sector = new BYTE[512];
+    int flag = ReadSector(drive, 0, sector);
+ 
+    // nếu đọc thành công
+    if (flag == 1)
+    {
+        // in ra bảng sector vừa đọc
+        Print_Sector(sector);
+
+        // đọc Partition Boot Sector
+        Read_PBS(sector);
+
+        // giải phóng bộ nhớ
+        delete[] sector;
+    }
+    else return 0;
+}
